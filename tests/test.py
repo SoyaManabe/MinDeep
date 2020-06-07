3591b74dda13781e1df99a9ba57c094a27483b38
SoyaManabe <>
2020-06-07 23:22:20 +0900

    add test ci yaml file

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,25 @@
-import main
 import unittest
 import numpy as np
+import mindeep
 
 class SquareTest(unittest.TestCase):
     def test_forward(self):
-        x = main.Variable(np.array([2.0]))
-        y = main.square(x)
+        x = mindeep.Variable(np.array([2.0]))
+        y = mindeep.square(x)
         expected = np.array([4.0])
         self.assertEqual(y.data, expected)
     
     def test_backward(self):
-        x = main.Variable(np.array([3.0]))
-        y = main.square(x)
+        x = mindeep.Variable(np.array([3.0]))
+        y = mindeep.square(x)
         y.backward()
         expected = np.array([6.0])
         self.assertEqual(x.grad, expected)
 
     def test_gradient_check(self):
-        x = main.Variable(np.random.rand(1))
-        y = main.square(x)
+        x = mindeep.Variable(np.random.rand(1))
+        y = mindeep.square(x)
         y.backward()
-        num_grad = main.numerical_diff(main.square, x)
+        num_grad = mindeep.numerical_diff(mindeep.square, x)
         flg = np.allclose(x.grad, num_grad)
         self.assertTrue(flg)
